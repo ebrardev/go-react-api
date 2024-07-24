73851b49d878a6e3bc6cc75c672f088b1c821c92
ebrardev <16155052@mersin.edu.tr>
2024-07-24 21:38:49 +0300

    variables

--- main.go
+++ main.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+	// define a variable
+	var myName string = "John Doe"
+
+	// define a constant
+	const mySecondName string = "Jane Doe"
+	// shorthand
+	myThirdName := "Bob Doe"
+
+	fmt.Println(myName)
+	fmt.Println(mySecondName)
+	fmt.Println(myThirdName)
 }
