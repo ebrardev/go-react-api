c2054191aece0542aa39259ac5ac92ff650cc77c
ebrardev <16155052@mersin.edu.tr>
2024-07-26 17:20:29 +0300

    static components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import {  Container, Stack } from "@chakra-ui/react"
 import Navbar from "./components/Navbar"
 import TodoForm from "./components/TodoForm"
+import TodoList from "./components/TodoList"
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
      <Navbar />
      <Container>
        <TodoForm />
-      {/* <TodoList />  */}
+       <TodoList />  
      </Container>
     </Stack>
   )
